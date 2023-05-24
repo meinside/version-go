3b5329d081ed3a4379dac2e209335354d203109a
Sungjin Han <meinside@duck.com>
2023-05-24 12:31:57 +0900

    Add `Minimum` function

--- version_test.go
+++ version_test.go
@@ -5,6 +5,16 @@ import (
 )
 
 func TestAll(t *testing.T) {
+	full := Full()
+	if full == UnknownVersion {
+		t.Errorf("full build version is invalid")
+	}
+
+	min := Minimum()
+	if min == UnknownVersion {
+		t.Errorf("minimum build version is invalid")
+	}
+
 	build := Build(All)
 	if build == UnknownVersion {
 		t.Errorf("build version is invalid")
